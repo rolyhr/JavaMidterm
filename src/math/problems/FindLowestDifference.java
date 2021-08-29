545b5222b832ce009d50a831b4932e65788c5e63
“Roly” <hafizurroly@gmail.com>
2021-08-28 22:06:38 -0400

    First Commit

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -8,8 +8,9 @@ public class FindLowestDifference {
          HINT: The lowest difference between these arrays is 1
         */
 
-        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
+        int[] array1 = {30, 12, 5, 9, 2, 20, 33, };
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
+
 
     }
 
