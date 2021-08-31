f58697541cf1d9d7ef703640a83a84a861009078
“Roly” <hafizurroly@gmail.com>
2021-08-31 14:51:57 -0400

    Completed - Palindrome

--- src/st/problems/Palindrome.java
+++ src/st/problems/Palindrome.java
@@ -1,4 +1,4 @@
-package string.problems;
+package st.problems;
 
 public class Palindrome {
 
@@ -10,5 +10,20 @@ public class Palindrome {
             Write a method to check if a given String is a palindrome or not.
          */
 
+        System.out.println(isPalindrome("Mom"));
+
+    }
+
+    public static String isPalindrome(String text) {
+        String lowerCase = text.toLowerCase();
+        int forward = 0;
+        int backward = lowerCase.length() - 1;
+        while (backward > forward) {
+            char forwardChar = lowerCase.charAt(forward++);
+            char backwardChar = lowerCase.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return text = text + " is not a palindrome";
+        }
+        return text = text + " is a palindrome";
     }
 }
