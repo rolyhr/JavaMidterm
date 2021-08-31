651eab562b34249ffe3ef1dfa7ccbbab9d082d67
“Roly” <hafizurroly@gmail.com>
2021-08-31 14:50:37 -0400

    Completed - DuplicateWord

--- src/st/problems/DuplicateWord.java
+++ src/st/problems/DuplicateWord.java
@@ -1,4 +1,4 @@
-package string.problems;
+package st.problems;
 
 import java.util.Locale;
 import java.util.Objects;
@@ -12,60 +12,26 @@ public class DuplicateWord {
             Also Find the average length of the words.
          */
 
-//        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-//        st = st.toLowerCase(Locale.ROOT);
-//        String[] lowerCaseWords = st.split("\\s");
-//
-//        int count = 0;
-//        for (int i = 0; i < lowerCaseWords.length; i++) {
-//            count = 1;
-//            for (int j = i+1; j < lowerCaseWords.length; j++) {
-//                if (lowerCaseWords[i].equals(lowerCaseWords[j])) {
-//                    count++;
-//                    lowerCaseWords[j] = "0";
-//                }
-//            }
-//            if(count > 1 && lowerCaseWords[i] != "0")
-//                System.out.println(lowerCaseWords[i]);
-//            System.out.println(count);
-//        }
+        String st = "Java is a programming Language Java is also an Island of Indonesia. Java is widely used language";
 
+        findDuplicateWords(st);
+    }
 
-
-        String string = "Big black bug bit a big black dog on his big black nose";
+    public static void findDuplicateWords(String str) {
         int count;
-
-        //Converts the string into lowercase
-        string = string.toLowerCase();
-
-        //Split the string into words using built-in function
-        String[] words = string.split(" ");
-
-        System.out.println("Duplicate words in a given string : ");
-        for(int i = 0; i < words.length; i++) {
+        String duplicates = "";
+        String[] lcWords = str.toLowerCase().trim().split("\\s");
+        for(int i = 0; i < lcWords.length; i++) {
             count = 1;
-            for(int j = i+1; j < words.length; j++) {
-                if(words[i].equals(words[j])) {
+            for (int j = i + 1; j < lcWords.length; j++) {
+                if (lcWords[i].equals(lcWords[j])) {
                     count++;
-                    //Set words[j] to 0 to avoid printing visited word
-                    words[j] = "0";
+                    lcWords[j] = "0";
                 }
             }
-
-            //Displays the duplicate word if count is greater than 1
-            if(count > 1 && !Objects.equals(words[i], "0"))
-                System.out.println(words[i]);
+            if(count > 1 && !lcWords[i].equals("0"))
+                System.out.println(lcWords[i]);
         }
-
-
-
-//        for (int i = 0; i < words.length; i++) {
-//            if (words[i].length() > maxLength) {
-//                maxLength = words[i].length();
-//                st = words[i];
-//            }
-//        }
-
     }
 
 }
