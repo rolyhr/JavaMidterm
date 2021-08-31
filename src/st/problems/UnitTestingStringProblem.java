6462a5bebc2aaf962ae0f17ad9cd9f1dce3f6ddd
“Roly” <hafizurroly@gmail.com>
2021-08-31 14:52:39 -0400

    Completed - UnitTestingStringProblem

--- src/st/problems/UnitTestingStringProblem.java
+++ src/st/problems/UnitTestingStringProblem.java
@@ -1,8 +1,40 @@
-package string.problems;
+package st.problems;
+
+import org.testng.Assert;
+
+import java.util.Map;
 
 public class UnitTestingStringProblem {
 
     public static void main(String[] args) {
-        // Unit testing for all classes within this package should be implemented here
+        UnitTestingStringProblem unitTest = new UnitTestingStringProblem();
+
+        unitTest.testAnagram();
+        unitTest.testDetermineLargestWord();
+        unitTest.testIsPalindrome();
     }
+
+    public void testAnagram() {
+        boolean actualResult = Anagram.isAnagram("cat", "aCt");
+        boolean expectedResult = true;
+        Assert.assertEquals(actualResult, expectedResult, "Failed - testAnagram");
+    }
+
+    public void testDetermineLargestWord() {
+        String s = "Human brain is a biological learning machine";
+
+        Map<Integer, String> actualResult = DetermineLargestWord.findTheLargestWord(s);
+        String expectedResult = "10 biological";
+        Assert.assertEquals(actualResult, expectedResult, "Failed - testDetermineLargestWord");
+    }
+
+    public void testIsPalindrome() {
+        String s = "Hannah";
+
+        String actualResult = Palindrome.isPalindrome(s);
+        String expectedResult = "Mom is a palindrome";
+        Assert.assertEquals(actualResult, expectedResult, "Failed - testIsPalindrome");
+    }
+
+
 }
