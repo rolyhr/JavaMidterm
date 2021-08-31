705752714cb25af5ccd5d149676717af52c4b75c
“Roly” <hafizurroly@gmail.com>
2021-08-31 15:29:05 -0400

    Completed - LowestNumber

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -2,6 +2,7 @@ package math.problems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import databases.ConnectToSqlDB;
 
@@ -12,9 +13,24 @@ public class LowestNumber {
          Write a method to find the lowest number from this array.
          */
 
-        int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
+        int[] numArray = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
 
+        System.out.println(findLowestNumber(numArray));
+    }
 
+    public static int findLowestNumber(int[] array) {
+        int i, j, key, temp;
+        for (i = 1; i < array.length; i++) {
+            key = array[i];
+            j = i - 1;
+            while (j >= 0 && key < array[j]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                j--;
+            }
+        }
+        return array[0];
     }
 
 }
