eec6f7e386146e54ee9d26d37da69b1ee0b4c1b7
“Roly” <hafizurroly@gmail.com>
2021-09-01 12:53:31 -0400

    Added execution time

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -73,7 +73,6 @@ public class Numbers {
     /*
     HELPER METHODS
      */
-
     public static void storeRandomNumbers(int[] num) {
         Random rand = new Random();
         for (int i = 0; i < num.length; i++) {
